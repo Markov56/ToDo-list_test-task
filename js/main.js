0de0aff6510da129c12f50735a763b97633e8b3a
Mark Markov <mkdir.user.name77@gmail.com>
2021-11-09 05:44:42 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,89 @@
+const todoInput = document.querySelector('.todo__input');
+const todoBtn = document.querySelector('.todo__btn');
+const todoList = document.querySelector('.todo__list');
+
+let todos = [];
+
+const postTodo = (newObj) => {
+  fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    body: JSON.stringify(newObj),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+if (localStorage.getItem('list')) {
+  todos = JSON.parse(localStorage.getItem('list'));
+  renderItems();
+}
+
+const countId = () => {
+  let lastId = todos[todos.length - 1] || 0;
+  console.log(lastId);
+};
+
+const onAddClick = () => {
+  if (todoInput.value) {
+    const newTask = {
+      description: todoInput.value,
+      done: false,
+    };
+
+    if (!todos.find((item) => item.description == newTask.description)) {
+      todos.push(newTask);
+      localStorage.setItem('list', JSON.stringify(todos));
+      renderItems();
+      todoInput.value = '';
+    } else {
+      alert('Вы уже добавили эту задачу');
+    }
+  }
+};
+
+const onRemoveClick = (event) => {
+  let elementForRemove = event.target.parentElement;
+  console.dir(elementForRemove.getAttribute('id'));
+  let elementIndex = elementForRemove.getAttribute('id');
+  todos.splice(elementIndex, 1);
+  localStorage.setItem('list', JSON.stringify(todos));
+  elementForRemove.remove();
+  renderItems();
+};
+
+todoBtn.addEventListener('click', onAddClick);
+todoInput.addEventListener('keypress', (keyPressed) => {
+  if (keyPressed.which == 13) {
+    onAddClick();
+  }
+});
+
+function renderItems() {
+  let tasks = '';
+  todos.map((item, index) => {
+    tasks += `
+        <li class="todo__list-item" id=${index}>
+            <input type="checkbox" id=${index} ${item.done ? 'checked' : ''}/>
+            <label for=${index}>${item.description}</label>
+            <button class="todo__list__remove" onclick="onRemoveClick(event)">
+              
+            </button>
+        </li>
+        `;
+    todoList.innerHTML = tasks;
+  });
+}
+
+todoList.addEventListener('change', (event) => {
+  let id = event.target.getAttribute('id');
+  let labelText = todoList.querySelector(`[for="${id}"]`).innerHTML;
+  todos.forEach((item) => {
+    if (item.description == labelText) {
+      item.done = !item.done;
+      localStorage.setItem('list', JSON.stringify(todos));
+    }
+  });
+});
